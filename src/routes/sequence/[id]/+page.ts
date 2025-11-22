fbf27e0fa915ddc425e34687761a1320bed4733f
Ruan <ruan.klein@gmail.com>
2025-11-21 22:25:27 -0300

    fix: spa in production mode

--- src/routes/sequence/[id]/+page.ts
+++ src/routes/sequence/[id]/+page.ts
@@ -1,6 +1,9 @@
 import type { PageLoad } from './$types';
 import type { ManifestEntry } from '$lib/types';
 
+// Disable prerendering for this dynamic route
+export const prerender = false;
+
 export const load: PageLoad = async ({ params, fetch }) => {
 	try {
 		const response = await fetch('/manifest.json');
